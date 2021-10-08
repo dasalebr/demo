3c29230c63ae30812a621823da86e028c6f6d0f1
dasalebr <65932591+dasalebr@users.noreply.github.com>
2021-10-08 13:26:13 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,48 +1,39 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 3.11"
-    }
+# Test case: Resource (ec2) use default sg (automatically created) in a newly created VPC
+# Expected: alert on the use of default sg
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+locals {
+  test_description = "resource (ec2) use default sg (automatically created) in a newly created VPC"
+  test_name        = "Integration test - use case 1"
+  cidr_block       = "10.9.0.0/16"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = local.cidr_block
+
+  tags = {
+    Name = local.test_name
   }
 }
 
-provider "aws" {
-  region = var.region
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = local.cidr_block
+
+  tags = {
+    Name = local.test_name
+  }
 }
 
-module "network" {
-  source       = "./modules/network"
-  environment  = var.environment
-  default_tags = var.default_tags
-}
+resource "aws_instance" "ec2" {
+  ami           = "ami-07cda0db070313c52"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.subnet.id
 
-module "storage" {
-  source         = "./modules/storage"
-  private_subnet = module.network.private_subnet
-  vpc_id         = module.network.vpc_id
-  environment    = var.environment
-  default_tags   = var.default_tags
-  db_username    = var.db_username
-  db_password    = var.db_password
-}
-
-module "compute" {
-  source = "./modules/compute"
-  environment = var.environment
-  region = var.region
-  elb_target_group_arn = module.network.target_lb_group_arn
-  private_subnet = module.network.private_subnet
-  public_subnet = module.network.public_subnet
-  elb_sg = module.network.target_lb_security_group
-  elb_url = module.network.elb_url
-}
-
-resource "local_file" "web-access" {
-  content  = <<JSON
-{
-  "fqdn": "${module.network.elb_url}"
-}
-  JSON
-  filename = "./web-access.json"
+  tags = {
+    Name = local.test_name
+  }
 }
